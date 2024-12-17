30599cddda73ae5b705e410c60f46cb8dc0f18f8
LAPTOP-63SV8TPF\LENOVO <nishedha.srilak@gmail.com>
2024-12-17 10:19:15 +0530

    add direction layer

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,9 +1,105 @@
-import React from 'react'
+import TrashBinCard from '@components/card';
+import React, { useEffect, useState, useCallback } from 'react';
+import './index.scss';
+import { DirectionsRenderer, GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
+import { deprecations } from 'sass';
 
-const GoogleMapContainer = () => {
+const containerStyle = {
+  width: "100%",
+  height: "100%",
+};
+
+
+const DirectionContainer: React.FC = () => {
+  const [map, setMap] = useState<google.maps.Map | null>(null);
+  const [center, setCenter] = useState<google.maps.LatLngLiteral | null>(null);
+  const [directionsResponse, setDirectionsResponse] = useState<google.maps.DirectionsResult | null>(null)
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      setCenter({ lat: 6.053519, lng: 80.220978 });
+    });
+  }, []);
+
+  const onLoad = useCallback((map: google.maps.Map) => {
+    if (center) {
+      const bounds = new google.maps.LatLngBounds(center);
+      map.fitBounds(bounds);
+    }
+    setMap(map);
+    map.setZoom(10)
+  }, [center]);
+
+  const onUnmount = useCallback(() => {
+    setMap(null);
+  }, []);
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY ?? "",
+  });
+
+  async function calculateRoute() {
+
+    const directioValue = { lat: 6.142658661791, lng: 80.54002282138 }
+    if (!center && !directioValue) {
+      return
+    }
+    if (center) {
+      const directionsService = new google.maps.DirectionsService()
+      const results = await directionsService.route({
+        origin: center,
+        destination: directioValue,
+        // eslint-disable-next-line no-undef
+        travelMode: google.maps.TravelMode.DRIVING,
+      })
+      setDirectionsResponse(results)
+    }
+  }
+
   return (
-    <div>index</div>
-  )
-}
+    <div className='login-container p-4'>
+      <div className="map-input-container d-flex gap-2 mb-4">
+        <div className="d-flex gap-2">
+          <button type="button" className="btn btn-success" onClick={() => calculateRoute()}>Calculate Route</button>
+          <button type="button" className="btn btn-success" onClick={() => map?.panTo(center!)}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
+              <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z" />
+            </svg>
+          </button>
+          {/* <button type="button" className="btn btn-success" onClick={() => setDirectionsResponse(null)} style={{ marginLeft: 'auto' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16">
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+            </svg>
+          </button> */}
+        </div>
+      </div>
+      <div className="map-wrapper">
+        {isLoaded && center && (
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+            options={{
+              // zoomControl: false,
+              streetViewControl: false,
+              mapTypeControl: false,
+              fullscreenControl: false,
+            }}
+          >
+            <Marker position={center} />
+            {directionsResponse && (
+              <DirectionsRenderer directions={directionsResponse} />
+            )}
+          </GoogleMap>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default GoogleMapContainer
+export default DirectionContainer;
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,37 +1,67 @@
 import { AddTrashBin, FindTrashBin, TrashBin } from '@assets/img';
 import TrashBinCard from '@components/card';
 import './landing.scss'
-import React from 'react'
+import React, { useState } from 'react'
 import { useAppSelector } from '@hooks/hooks';
 import { binListGetter, trashTypeGetter } from '@core/coreSlice';
 import { TrashBinRes } from '@core/interface';
 import { TRASH_TYPES } from '@constant/index';
-import PlacesAutocomplete from 'react-places-autocomplete';
 import GeocodingAutocomplete from '@components/autocomplete';
-
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import DirectionContainer from '@components/map';
 const TrashBinList = () => {
   const binList = useAppSelector(binListGetter);
   const trashgType = useAppSelector(trashTypeGetter);
+  const [show, setShow] = useState(false);
+  const [completeDispose, setCompleteDispose] = useState(false);
 
-const binHighlight = (bin: TrashBinRes)=>{
-  if(bin.glass && trashgType == TRASH_TYPES.GLASS) return 'glass'
-  if(bin.paper && trashgType == TRASH_TYPES.PAPER) return 'paper'
-  if(bin.plastic && trashgType == TRASH_TYPES.PLASTIC) return 'plastic'
-  if(bin.organic && trashgType == TRASH_TYPES.ORGANIC) return 'organic'
-  else return ''
-}
+  const binHighlight = (bin: TrashBinRes) => {
+    if (bin.glass && trashgType == TRASH_TYPES.GLASS) return 'glass'
+    if (bin.paper && trashgType == TRASH_TYPES.PAPER) return 'paper'
+    if (bin.plastic && trashgType == TRASH_TYPES.PLASTIC) return 'plastic'
+    if (bin.organic && trashgType == TRASH_TYPES.ORGANIC) return 'organic'
+    else return ''
+  }
 
-const trashTypeRender = (bin: TrashBinRes)=>{
-  let trash = '';
-  if(bin.glass) trash += 'glass '
-  if(bin.paper)  trash += 'paper '
-  if(bin.plastic) trash +=  'plastic '
-  if(bin.organic) trash +=  'organic '
-  
-  return trash
-}
+  const trashTypeRender = (bin: TrashBinRes) => {
+    let trash = '';
+    if (bin.glass) trash += 'glass '
+    if (bin.paper) trash += 'paper '
+    if (bin.plastic) trash += 'plastic '
+    if (bin.organic) trash += 'organic '
+
+    return trash;
+  }
+
+  const findRouteClick = () => {
+    setShow(true);
+  }
+
   return (
     <>
+      <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        centered
+        dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+
+        {!completeDispose ? <div>
+          <DirectionContainer />
+          <div className='px-4 pb-4'>
+            <Button variant="success" className='mt-2 w-100' onClick={() => setCompleteDispose(true)}>
+              Complete Trash Dispose
+            </Button>
+          </div>
+        </div> :
+          <div className='p-4'>
+            <h4 className='text-success mb-2'>Give feedback & Review</h4>
+            <Button variant="success" className='mt-2 w-100' onClick={() => { setShow(false); setCompleteDispose(false) }}>
+              Complete
+            </Button></div>}
+      </Modal>
       {binList && binList.length > 0 ?
         binList.map((bin: TrashBinRes) =>
           <TrashBinCard key={bin.id}>
@@ -42,13 +72,13 @@ const trashTypeRender = (bin: TrashBinRes)=>{
                     <img className='home-trash-bin-img-wrapper' src={bin.imageUrl} />
                   </div>
                   <div style={{ backgroundImage: `url(${TrashBin})` }} className='list-trash-bin pt-3'>
-                    <button type="button" className="btn btn-outline-secondary mt-4">Find Route</button>
+                    <button type="button" className="btn btn-outline-secondary mt-4" onClick={findRouteClick}>Find Route</button>
                   </div>
                   <div className='list-trash-bin-text-container d-flex flex-column justify-content-center align-items-center gap-2 mt-6'>
-                    <GeocodingAutocomplete initialLat={bin.latitude} initialLng={bin.longitude} disabled/>
+                    <GeocodingAutocomplete initialLat={bin.latitude} initialLng={bin.longitude} disabled />
                     <p >
                       Trash Type Available to dispose
-                      <br/>
+                      <br />
                       {trashTypeRender(bin)}
                     </p>
                   </div>
